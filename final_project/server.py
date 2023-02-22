ead0ee036b2a3a82b49ea533d29241547d687ed0
AbdalazizSayed <abdaluziz.s98@gmail.com>
2023-02-22 20:48:42 +0200

    if condition ide

--- final_project/server.py
+++ final_project/server.py
@@ -28,7 +28,7 @@ def french_to_english(text1):
 def renderIndexPage():
     # Write the code to render template
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8080)
 
 ##printing 
